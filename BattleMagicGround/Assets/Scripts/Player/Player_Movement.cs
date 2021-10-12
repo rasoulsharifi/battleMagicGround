8ed3d6c73d55c5035bc72fd9b3420f38daa20db7
Rasoul Sharifi <rasoul.sharifi1988@gmail.com>
2021-10-12 17:12:51 +0330

    create new project

---
+++ BattleMagicGround/Assets/Scripts/Player/Player_Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    // variables
+    [SerializeField] private float playerSpeed;
+
+    // references
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(x, 0, z);
+        transform.position += movement * Time.deltaTime * playerSpeed;
+
+           
+
+        if (movement != Vector3.zero)
+        {
+            anim.SetBool("run", true);
+
+            transform.rotation = Quaternion.Slerp
+                (transform.rotation, Quaternion.LookRotation(movement), .3f);
+        }
+        else
+        {
+            anim.SetBool("run", false);
+        }
+
+        
+     }
+}
